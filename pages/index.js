5737a0a3404c845708f2d10de9324c73c5a49953
Juan Jose Bernal Villamarin <juanjosebernalvillamarin@gmail.com>
2023-03-28 19:40:51 -0500

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,19 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Raccoon Pokemon</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        <h1 className="title">
+          Read <Link href="/posts/first-post">this page!</Link>
+          {/* <a href="/posts/first-post">RBD</a> */}
         </h1>
 
         <p className={styles.description}>
@@ -111,5 +113,5 @@ export default function Home() {
         }
       `}</style>
     </div>
-  )
+  );
 }
---
+++ pages/nest/page_1/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Page_1 = () => {
+  return (
+    <div>Page_1</div>
+  );
+};
+
+export default Page_1;
